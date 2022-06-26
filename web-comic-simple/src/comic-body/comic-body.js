40464220d4082e1f2d1b3a9e314548c7860c3ad7
Joseph Richardson <jrichardson@jmstudios.net>
2022-06-26 15:07:28 -0700

    initial commit

---
+++ web-comic-simple/src/comic-body/comic-body.js
@@ -0,0 +1,7 @@
+import "./comic-body.css";
+
+function ComicBody() {
+  return <div className="comic-body">Body pages with the navs</div>;
+}
+
+export default ComicBody;
---
+++ web-comic-simple/src/header/header.js
@@ -0,0 +1,11 @@
+import "./header.css";
+
+function Header() {
+  return (
+    <header className="Header">
+      <h1>Tales From the Leayline Void</h1>
+    </header>
+  );
+}
+
+export default Header;
