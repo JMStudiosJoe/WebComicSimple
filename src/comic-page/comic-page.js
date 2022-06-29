ad9c368a983c616e3daca5af7438b96cfc2b4207
erikavasnormandy <erikavasnormandy@gmail.com>
2022-06-28 23:14:19 -0700

    font and background

---
+++ src/comic-page/comic-page.js
@@ -0,0 +1,13 @@
+import "./comic-page.css";
+import './fonts/ZagBold.otf'
+import './fonts/ZagRegular.otf'
+
+function ComicPage({ page }) {
+  return (
+    <section className="comic-page">
+      <img src={page} />
+    </section>
+  );
+}
+
+export default ComicPage;
